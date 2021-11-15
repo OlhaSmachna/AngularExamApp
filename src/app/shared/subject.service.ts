8a7d35cb6322fbfcccb26bc6ffe00725297afa93
Olga Smachna <smachna.ov@gmail.com>
2021-11-15 12:25:16 +0200

    Login finished

---
+++ src/app/shared/subject.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServerService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  private async request(method: string, url: string, data?: any) {
+
+    const result = this.http.request(method, url, {
+      body: data,
+      responseType: 'json',
+      observe: 'body',
+      headers: {
+      }
+    });
+    return new Promise((resolve, reject) => {
+      result.subscribe(resolve, reject);
+    });
+  }
+
+  getChars() {
+    return this.request('GET', `${environment.serverUrl}/event`);
+  }
+
+  // createChars(event: { name: any; description: any; date: any; }) {
+  //   return this.request('POST', `${environment.serverUrl}/event`, event);
+  // }
+  //
+  // updateChars(event: { id: any; name?: any; description?: any; date?: any; }) {
+  //   return this.request('PUT', `${environment.serverUrl}/event/${event.id}`, event);
+  // }
+  //
+  // deleteChars(event: { id: any; }) {
+  //   return this.request('DELETE', `${environment.serverUrl}/event/${event.id}`);
+  // }
+}
